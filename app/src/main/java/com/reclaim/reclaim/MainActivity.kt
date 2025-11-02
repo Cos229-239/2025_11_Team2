2590d555aaf0fdf40ae4f0dd3428c9065eae8d73
Justin Greenwood <JDGreenwood@student.fullsail.edu>
2025-11-01 22:16:55 -0500

    Justin initial push

--- app/src/main/java/com/reclaim/reclaim/MainActivity.kt
+++ app/src/main/java/com/reclaim/reclaim/MainActivity.kt
@@ -3,11 +3,15 @@ package com.reclaim.reclaim
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import com.reclaim.reclaim.navigation.NavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+            NavGraph(navController)
         }
     }
 }
