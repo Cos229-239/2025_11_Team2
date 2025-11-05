7663923d86fc4b58e0c754d4e01b4f302a06087a
George <datguysolid@gmail.com>
2025-11-04 16:36:44 -0800

    Reclaim dev - Coping Strategies screen created. Functions properly.

--- app/src/main/java/com/reclaim/reclaim/MainActivity.kt
+++ app/src/main/java/com/reclaim/reclaim/MainActivity.kt
@@ -3,15 +3,23 @@ package com.reclaim.reclaim
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.reclaim.reclaim.navigation.NavGraph
+import com.reclaim.reclaim.ui.theme.ReclaimTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-            NavGraph(navController)
+            ReclaimTheme { // Use your custom theme wrapper
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    NavGraph(navController)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/reclaim/reclaim/navigation/NavGraph.kt
+++ app/src/main/java/com/reclaim/reclaim/navigation/NavGraph.kt
@@ -6,7 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.reclaim.reclaim.ui.home.HomeScreen
 import com.reclaim.reclaim.ui.trigger.TriggerMapScreen
-
+import com.reclaim.reclaim.ui.coping.CopingStrategiesScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
@@ -16,7 +16,9 @@ fun NavGraph(navController: NavHostController) {
         }
         composable(Screen.Map.route) {
             TriggerMapScreen(navController)
-
+        }
+        composable(Screen.Strategies.route) {
+            CopingStrategiesScreen(onBackClick = { navController.popBackStack() })
         }
     }
 }
