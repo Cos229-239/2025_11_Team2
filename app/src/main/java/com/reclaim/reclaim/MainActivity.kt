342c2fe688b642fe1121c5f5f772f76416a5b1b7
Mya Coleman <myamrc455@outlook.com>
2025-11-04 17:43:00 -0800

    mya's personal

--- app/src/main/java/com/reclaim/reclaim/MainActivity.kt
+++ app/src/main/java/com/reclaim/reclaim/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.reclaim.reclaim.navigation.NavGraph
+import com.reclaim.reclaim.ui.journal.JournalScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/reclaim/reclaim/ui/theme/Color.kt
+++ app/src/main/java/com/reclaim/reclaim/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Teal1 = Color(color = 0xFF00A991)
+
+val TeaL2 = Color(color = 0xFF00B4A0)
+
+val Sand = Color(color = 0xFFE8D2B7)
